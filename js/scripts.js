22437540c7b785237658ae0d60cbfdb5012239fe
jz3309 <43050480+jz3309@users.noreply.github.com>
2019-04-11 16:38:09 -0400

    add more description

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,11 @@ $('#mainpart').text("August 2018 - August 2019: Master - New York University ; A
 
 
 $("#PROJECT").on("click",function(){
-$('#mainpart').text('NYPD Vehicle Collision Analysis')
+$('#mainpart').text('NYPD Vehicle Collision Analysis： We looked at the NYPD Motor Vehicle Collisions Dataset available on NYC Open Data to explore the relationship of New York City vehicle accident casualties with various other factors.The conclusion is that time plays a crucial role in predicting accidents. As for locations, different locations might have very different accidents profiles. But the more data we have, the more apparent the temporal patterns become. In the future, it might be worthy to try different spatial properties to identify clusters. Also, it will also be interesting to investigate how meaningful are the clusters regarding their explaining power.')
 })
 
 $("#internship").on("click",function(){
-$('#mainpart').text('Internship : South China Sea Think Tank')
+$('#mainpart').text('Internship : South China Sea Think Tank   I used ArcGIS and Google Earth to accurately calculate the coordinates of territorial sea baselines for the interactive map.')
 })
 
 $("#facebook").on('click',function() {
